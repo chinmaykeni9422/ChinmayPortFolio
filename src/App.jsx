05c324cb224e551a1f63a5631a5b48ff4b1fb777
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-10 11:18:45 +0530

    App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import SideNavBar from './components/NavBar/SideNavBar'
+import Main from './components/Main/Main'
 
 function App() {
 
   return (
     <>
       <SideNavBar />
+      <Main/>
     </>
   )
 }
